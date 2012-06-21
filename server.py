ff52592a392b16251b088d08e40c87b15c242a41
Nicolas Hery <nicolahery@gmail.com>
2012-06-21 14:38:09 -0400

    Add excel template loading

--- server.py
+++ server.py
@@ -4,21 +4,45 @@ from urlparse import urlsplit, urlunsplit
 
 from flask import Flask, render_template, request, jsonify
 from pymongo import Connection
+from gridfs import GridFS
+from bson.objectid import ObjectId
 
 # Envrionment variables for config
 mongodb_uri = os.environ.get('MONGOLAB_URI', 'mongodb://localhost/excesiv')
 
-# Initialize
+# Other config
+APP_DEBUG = True
+TASK_TIMEOUT = 5 # How long to wait for task to finish (in seconds) 
+
+# Setup
 app = Flask(__name__)
 connection = Connection(mongodb_uri)
 db_name = urlsplit(mongodb_uri).path.strip('/')
 db = connection[db_name]
 tasks = db.tasks
 results = db.results
+fs = GridFS(db)
+fs_meta = db.fs.files
+app.debug = APP_DEBUG
 
-app.debug = True
-
-TASK_TIMEOUT = 5 # How long to wait for task to finish (in seconds) 
+def load_excel_templates():
+    # Drop all templates currently in GridFS
+    for meta in fs_meta.find({'template': True}):
+        filename = meta['filename']
+        oid = meta['_id']
+        fs.delete(ObjectId(oid))
+        print 'Dropped template %s' % filename
+    # Load all Excel templates in the 'excel' directory
+    excel_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 'excel')
+    for filename in os.listdir(excel_dir):
+        _, extension = os.path.splitext(filename)
+        if extension == '.xlsx':
+            filepath = os.path.join(excel_dir, filename)
+            f = open(filepath, 'rb')
+            fs.put(f, filename=filename, template=True)
+            f.close()
+            print 'Loaded template %s' % filename
 
 @app.route('/')
 def index():
@@ -66,6 +90,9 @@ def demo():
         return ('Task timed out', 504)
     # Process result
     return jsonify({'message': doc['message']})
+
+# Initialize
+load_excel_templates()
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
