ff13e9880f167f4477bde37ddd62d4799de89f57
Nicolas Hery <nicolahery@gmail.com>
2012-09-12 17:09:59 -0400

    Allow app to run on other ports

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ xs.register_task_method('read', 'demo', demo_read)
 # RUN
 # -----------------------------------------------
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
